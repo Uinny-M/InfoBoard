eca84d64164c81973cc0a7517b129b511af6f096
Uinny-M <margaret_ne@rambler.ru>
2021-06-17 00:33:15 +0300

    add front, remove duplicate options

--- src/main/java/com/example/InfoBoard/Service/RestService.java
+++
@@ -1,9 +0,0 @@
-//package com.example.InfoBoard.Service;
-//
-//import com.example.InfoBoard.dto.EventOutDTO;
-//
-//import java.util.List;
-//
-//public interface RestService {
-//    List<EventOutDTO> getEvents();
-//}
